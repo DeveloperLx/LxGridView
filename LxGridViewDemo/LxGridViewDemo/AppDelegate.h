65c7408456536811e36e7a3f29b2e956af497209
lixian <lixian@git.etiantian.com>
2015-06-30 23:11:18 +0800

    raw version

---
+++ LxGridViewDemo/LxGridViewDemo/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  LxGridViewDemo
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LxGridViewDemo/LxGridViewDemo/ViewController.h
@@ -0,0 +1,11 @@
+//
+//  ViewController.h
+//  LxGridViewDemo
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+@end
